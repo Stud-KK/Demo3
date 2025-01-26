50a386bc576371d7ca4b7e4f04883d08db719e26
komal <komalkalshetti989@gmail.com>
2025-01-26 22:42:48 +0530

    Two files add in Feature1 branch

---
+++ Function1.java
@@ -0,0 +1,6 @@
+public class Function1 {
+    public static void main(String[] args) {
+        System.out.println("Hey there");
+    }
+}
+
---
+++ Function2.java
@@ -0,0 +1,8 @@
+public class Function2 {
+    
+    public static void main(String[] args) {
+        System.out.println("hello from function2");
+
+    }
+}
+
